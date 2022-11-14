93d9cf5eb357e433413c631b07d2086e9d0a9ca0
Maria Andrello <maa.andrello@hotmail.com>
2022-11-14 14:08:17 +0100

    01

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,14 @@
+class CoachingController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    return "yfyugyfu"
+    # if question.end_with?('?') == true
+    #   'Silly question, get dressed and go to work!'
+    # else
+    #   "I don't care, get dressed and go to work!"
+    # end
+  end
+end
